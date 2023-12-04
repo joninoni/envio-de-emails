c3a51ed0a1729aef90fe79106afaaa3bfce082c1
joninoni <jonibeltranhayer@gmail.com>
2023-12-04 11:45:03 -0600

    terminando las variables

--- js/copiaEmail.js
+++ js/copiaEmail.js
@@ -1,5 +1,9 @@
 //Variables
-
+const inputEmail=document.querySelector("#email");
+const inputAsunto=document.querySelector("#asunto");
+const inputMensaje=document.querySelector("#mensaje");
+const btnEnviar=document.querySelector(`button[type="submit"]`);
+const btnReset=document.querySelector(`button[type="reset"]`);
 //Eventos
 
 //Funciones
